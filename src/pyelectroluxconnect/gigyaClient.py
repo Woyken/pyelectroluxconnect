cf90fba0d38e77267a9d66be16a6c3f6e433415e
Woyken <13166802+Woyken@users.noreply.github.com>
2023-06-28 11:56:06 +0300

    added signature calculation

--- src/pyelectroluxconnect/gigyaClient.py
+++ src/pyelectroluxconnect/gigyaClient.py
@@ -2,13 +2,87 @@ import random
 import time
 from types import TracebackType
 from typing import Optional, Type
-from urllib.parse import urljoin
+from urllib.parse import urljoin, urlparse, quote_plus
 from aiohttp import ClientSession
+from json import dumps as jsonstringify
+import hmac
+from base64 import b64decode, b64encode
+from hashlib import sha1
+from math import floor
 
 from .gigyaModels import GetJWTResponse, LoginResponse, SocializeGetIdsResponse
 
 def current_milli_time():
     return round(time.time() * 1000)
+
+def UrlEncode(value):
+        if value is None:
+            return value
+        elif isinstance(value, int):
+            return str(value)
+        else:
+            if isinstance(value, dict) or isinstance(value, list):
+                str_value = jsonstringify(value)
+            else:
+                str_value = value.encode('utf-8')
+
+            return quote_plus(str_value).replace("+", "%20").replace("%7E", "~")
+
+def buildQS(params: dict):
+        """Converts a params dictionary to a sorted query string"""
+        queryString = ""
+        amp = ""
+        # keys = params.keys()
+        keys = list(params.keys())
+        keys.sort()
+        for key in keys:
+            value = params.get(key)
+            if value is not None:
+                queryString += amp + key + "=" + UrlEncode(value)
+                amp = "&"
+
+        return queryString
+
+def calcOAuth1BaseString(httpMethod: str, url: str, isSecureConnection: bool, requestParams: dict):
+        normalizedUrl = ""
+        u = urlparse(url)
+
+        if isSecureConnection:
+            protocol = "https"
+        else:
+            protocol = u.scheme.lower()
+
+        port = u.port
+
+        normalizedUrl += protocol + "://"
+        normalizedUrl += u.hostname.lower()
+
+        if port != None and ((protocol == "http" and port != 80) or (protocol == "https" and port != 443)):
+            normalizedUrl += ':' + port
+
+        normalizedUrl += u.path
+
+        # Create a sorted list of query parameters
+        queryString = buildQS(requestParams)
+
+        # Construct the base string from the HTTP method, the URL and the parameters
+        baseString = httpMethod.upper() + '&' + UrlEncode(normalizedUrl) + '&' + UrlEncode(queryString)
+
+        return baseString
+
+def calcSignature(baseString: str, secretKey: str):
+    encodedBase = baseString.encode("utf-8")
+    encodedKey = secretKey.encode('utf-8')
+    rawHmac = hmac.new(b64decode(encodedKey), encodedBase, sha1).digest()
+    signature = b64encode(rawHmac)
+    signature = signature.decode('utf-8')
+
+def getOAuth1Signature(secretKey: str, httpMethod: str, url: str, isSecureConnection: bool, requestParams: dict):
+        # Taken from https://github.com/SAP/gigya-python-sdk/blob/main/GSSDK.py#L276
+        # Create the BaseString.
+        baseString = calcOAuth1BaseString(httpMethod, url, isSecureConnection, requestParams)
+
+        return calcSignature(baseString, secretKey)
 
 class GigyaClient:
     def __init__(self, client_session: ClientSession, domain: str, api_key: str) -> None:
@@ -42,10 +116,10 @@ class GigyaClient:
             data: SocializeGetIdsResponse = await response.json()
             return data
 
-    async def login(self, gmid: str, username: str, password: str, ucid: str):
+    async def login(self, username: str, password: str, gmid: str, ucid: str):
         #https://accounts.eu1.gigya.com/accounts.login
         url = f'https://accounts.{self._domain}/accounts.login'
-        async with await self._client_session.get(
+        async with await self._client_session.post(
             url,
             data={
                 "apiKey": self._api_key,
@@ -63,27 +137,28 @@ class GigyaClient:
             data: LoginResponse = await response.json()
             return data
 
-    async def getJWT(self, gmid: str, sessionToken: str, ucid: str):
+    async def getJWT(self, sessionToken: str, sessionSecret: str, gmid: str, ucid: str):
         #https://accounts.eu1.gigya.com/accounts.getJWT
         url = f'https://accounts.{self._domain}/accounts.getJWT'
-        sig = "TODO"
-        timestampSeconds = "TODO"
-        async with await self._client_session.get(
+
+        dataParams = {
+            "apiKey": self._api_key,
+            "fields": "country",
+            "format": "json",
+            "gmid": gmid,
+            "httpStatusCodes": False,
+            "nonce": self._generate_nonce(),
+            "oauth_token": sessionToken,
+            "sdk": "Android_6.2.1",
+            "targetEnv": "mobile",
+            "timestamp": floor(time.time()),
+            "ucid": ucid
+        }
+        dataParams["sig"] = getOAuth1Signature(sessionSecret, "POST", url, True, data)
+
+        async with await self._client_session.post(
             url,
-            data={
-                "apiKey": self._api_key,
-                "fields": "country",
-                "format": "json",
-                "gmid": gmid,
-                "httpStatusCodes": False,
-                "nonce": self._generate_nonce(),
-                "oauth_token": sessionToken,
-                "sdk": "Android_6.2.1",
-                "sig": sig,
-                "targetEnv": "mobile",
-                "timestamp": timestampSeconds,
-                "ucid": ucid
-            }) as response:
+            data=dataParams) as response:
 
             data: GetJWTResponse = await response.json()
             return data
